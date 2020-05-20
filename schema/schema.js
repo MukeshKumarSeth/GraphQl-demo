da1ca5372db834edc31b109574c563d9f7099c4c
Mukesh Kumar Soni <ms153706@gmail.com>
2020-05-20 18:38:56 +0530

    changes

--- schema/schema.js
+++ schema/schema.js
@@ -1,28 +1,103 @@
 
-const {GraphQLObjectType, GraphQLSTring ,GraphQLSchema} = requre('graphql'); 
+const {GraphQLObjectType, 
+	GraphQLString ,
+	GraphQLSchema,
+    GraphQLInt,
+    GraphQLList ,
+    GraphQLID} = require('graphql'); 
+const ItemModel = require('../model/books.js');
 
-const BookType = new GraphQLObjectType({
+var author = [
+	{name:'Mukesh Kumar Soni',age:26,id:'1'},
+	{name:'Rahul Raj',age:24,id:'2'},
+	{name:'Bhawana Bist',age:28,id:'3'}
+]
+
+const BookType = new GraphQLObjectType({//to create book type object
 	name: "Book",
 	fields : ()=>({
-		id : {type:GraphQLSTring},
-	    name : {type:GraphQLSTring},
-		genre : {type:GraphQLSTring}
+		id : {type:GraphQLString},
+	    name : {type:GraphQLString},
+		genre : {type:GraphQLString}
 	})
 });
 
-const RootQuery = new GraphQLObjectType({
+const AuthorType = new GraphQLObjectType({
+	name: "Author",
+	fields : ()=>({
+		id : {type:GraphQLID},
+	    name : {type:GraphQLString},
+		age : {type:GraphQLInt}
+	})
+});
+
+const RootQuery = new GraphQLObjectType({//to execute
 	name: "RootQueryType",
 	fields :{
 		book:{//end point to feth from forntent
 			type:BookType,
-			arg : {id:{type : GraphQLSTring}},//it needed when we pass orgument from forntent
+			arg : {id:{type : GraphQLString}},//it needed when we pass orgument from forntent
 			resolve(){//this is a function which execute code to get data from db or other sources
 
 			}
+		},
+		author:{//end point to feth from forntent
+			type:new GraphQLList(AuthorType),
+			//arg : {id:{type : GraphQLID}},//it needed when we pass orgument from forntent
+			// resolve(){//this is a function which execute code to get data from db or other sources
+			// 	return find(authers,{id:arg.id}); 
+			// }
+			resolve: (parent,{ id })=>{
+				console.log('this is id',id);
+				const user = author.filter(author=>author.id == id);
+				return user[0];
+			}
 		}
 	}
-});
+})
 
 module.exports = new GraphQLSchema({
 	query: RootQuery
 });
+
+// const ItemType= new GraphQLObjectType({
+//  	name:"User_record",
+//  	fields : {
+//  		item:{
+// 			type:GraphQLString
+// 		},
+//  		qty: {
+//  			type:GraphQLInt
+//  		},
+//  	}
+// });
+
+// const  RootQuery = new GraphQLObjectType({
+// 		name:"Query",
+// 		fields:{
+// 			itemsList2:{
+// 				type: new GraphQLList(ItemType),
+// 				resolve: async (root, args, context, info)=>{
+// 					return await ItemModel.find().exec();//we can write query here related to db
+// 				}
+// 			},
+// 			addItem : {
+// 				type : ItemType,
+// 				args : {
+// 					item:{type: GraphQLString},
+// 					qty:{type: GraphQLInt}
+// 				},
+// 				resolve(parent,args){
+// 					let itemList = new ItemModel({
+// 						item :args.item,
+// 						qty : args.qty
+// 					});
+// 					return itemList.save();
+// 				}
+// 			}
+// 		}
+// 	})
+
+// module.exports = new GraphQLSchema({
+// 	query: RootQuery
+// });
--- server.js
+++ server.js
@@ -11,49 +11,13 @@ const app = express();
 const bodyParser = require('body-parser');
 const conn =  require('./config/connection.js');
 const ItemModel = require('./model/books.js');
-
-
-const ItemType= new GraphQLObjectType({
- 	name:"User_record",
- 	fields : {
- 		item:{
-			type:GraphQLString
-		},
- 		qty: {
- 			type:GraphQLInt
- 		},
- 	}
-});
+const schema = require('./schema/schema.js');
 
 
 
-const schema = new GraphQLSchema({
-	query: new GraphQLObjectType({
-		name:"Query",
-		fields:{
-			itemsList2:{
-				type: new GraphQLList(ItemType),
-				resolve: async (root, args, context, info)=>{
-					return await ItemModel.find().exec();//we can write query here related to db
-				}
-			},
-			addItem : {
-				type : ItemType,
-				args : {
-					item:{type: GraphQLString},
-					qty:{type: GraphQLInt}
-				},
-				resolve(parent,args){
-					let itemList = new ItemModel({
-						item :args.item,
-						qty : args.qty
-					});
-					return itemList.save();
-				}
-			}
-		}
-	})
-});
+
+
+
 
  app.use("/graphql",graphqlHTTP(
        {
